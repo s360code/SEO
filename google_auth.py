e465ac865bd89779547c78497e11f1baba71fb9b
StefanPedersen-s360 <58558438+StefanPedersen-s360@users.noreply.github.com>
2022-05-10 11:04:30 +0200

    removing class

--- google_auth.py
+++ google_auth.py
@@ -3,11 +3,11 @@ import gspread
 from oauth2client.client import GoogleCredentials
 from google.auth import default
 
-class Auth:
-  def __init___(self):
-    auth.authenticate_user()
-    creds, _ = default()
-  
-  def Authenticate(self):
-    gc = gspread.authorize(creds)
+
+def __init___(self):
+  auth.authenticate_user()
+  creds, _ = default()
+
+def Authenticate(self):
+  gc = gspread.authorize(creds)
 
