ba626fe48a2e77b581ce392a56f0f78604514f5d
StefanPedersen-s360 <58558438+StefanPedersen-s360@users.noreply.github.com>
2022-05-10 10:59:23 +0200

    Update google_auth.py

--- google_auth.py
+++ google_auth.py
@@ -7,8 +7,10 @@ from google.auth import default
 class Authenticating:
 
 
-  def __init___():
+  def __init___(self):
     auth.authenticate_user()
     creds, _ = default()
+  
+  def authenticate(self):
     gc = gspread.authorize(creds)
 
