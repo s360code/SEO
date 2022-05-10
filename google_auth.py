02dcfc8d32589bcce8eac4fddd292d1e705b7dec
StefanPedersen-s360 <58558438+StefanPedersen-s360@users.noreply.github.com>
2022-05-10 11:09:59 +0200

    changing imports

--- google_auth.py
+++ google_auth.py
@@ -1,8 +1,9 @@
+from google.colab import auth, output
+import gspread
+from oauth2client.client import GoogleCredentials
+from google.auth import default
+
 def Authenticate():
-  from google.colab import auth, output
-  import gspread
-  from oauth2client.client import GoogleCredentials
-  from google.auth import default
   auth.authenticate_user()
   creds, _ = default()
   gc = gspread.authorize(creds)
