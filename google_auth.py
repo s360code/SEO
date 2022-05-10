7ee627970bbd056f73dc0f8eeb1c4bf373cacdf1
StefanPedersen-s360 <58558438+StefanPedersen-s360@users.noreply.github.com>
2022-05-10 11:02:13 +0200

    Update google_auth.py

--- google_auth.py
+++ google_auth.py
@@ -1,16 +1,13 @@
 from google.colab import auth, output
 import gspread
 from oauth2client.client import GoogleCredentials
-
 from google.auth import default
 
-class Authenticating:
-
-
+class Auth:
   def __init___(self):
     auth.authenticate_user()
     creds, _ = default()
   
-  def authenticate(self):
+  def Authenticate(self):
     gc = gspread.authorize(creds)
 
